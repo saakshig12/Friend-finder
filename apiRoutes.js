a33372a42dd33c6bc38f84d6db6d5728ef8df367
Saakshig12 <saakshig12@gmail.com>
2019-08-05 19:30:39 -0400

    update

---
+++ apiRoutes.js
@@ -0,0 +1,10 @@
+var friends = require('../data/friends');
+
+module.exports = function (app) {
+
+    app.get('/api/friends', (req, res) => {
+        res.json(friends);
+        res.json(bestMatch);
+    })
+
+}
