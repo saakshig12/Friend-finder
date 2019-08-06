15367996fead7bd587e244033016bb14b38a4883
Saakshig12 <saakshig12@gmail.com>
2019-08-06 09:22:19 -0400

    html pages editted

---
+++ friends.js
@@ -0,0 +1,10 @@
+var friendsArr = [
+    {
+        name: "Saakshi",
+        photo: "",
+        scores: ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0"]
+    }
+    
+];
+
+module.exports = friendsArr;
