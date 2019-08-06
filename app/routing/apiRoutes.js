bd21b2ebe727f6c9d43091f06b489c4009cf4a89
Saakshig12 <saakshig12@gmail.com>
2019-08-06 18:57:43 -0400

    update

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,9 +2,21 @@ var friends = require('../data/friends');
 
 module.exports = function (app) {
 
-    app.get('/api/friends', (req, res) => {
-        res.json(friends);
+    app.get('./app/data/friends', function(req,res){
+        var bestMatch = req.params.friend;
+  
+    console.log(bestMatch);
+  
+    for (var i = 0; i < friends.length; i++) {
+      if (bestMatch === friends[i].routeName) {
+        return res.json(friends[i]);
+      }
+    }
+  
+    return res.json(false);
+  });
+}
 
-    })
+
+
     
-};
