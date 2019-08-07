090798cf55e136d0accc6c157c7993d2ac2a8105
Saakshig12 <saakshig12@gmail.com>
2019-08-07 09:39:09 -0400

    updates

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,21 +2,42 @@ var friends = require('../data/friends');
 
 module.exports = function (app) {
 
-    app.get('./app/data/friends', function(req,res){
-        var bestMatch = req.params.friend;
+    app.get('/app/data/friends', function(req,res){
+        var match = req.params.friends;
   
-    console.log(bestMatch);
+    console.log(match);
   
     for (var i = 0; i < friends.length; i++) {
-      if (bestMatch === friends[i].routeName) {
+      if (match === friends[i].routeName) {
         return res.json(friends[i]);
       }
     }
-  
     return res.json(false);
   });
 }
+app.post("/app/data/friends", function(req, res) {
+    var scoreCal;
+        var userData = req.body;
+        var match = {
+            name: "",
+            photo: "",
+            scores: "",
+        }
+        for (var i = 0; i < friends.length; i++) {
+            scoreCal = 0;
+            for (var i = 0; i < Friend[i].scores.length; i++) {
+                var friendScores = userData.scores[i];
+                var FriendScore = Friend[i].scores[i];
 
-
-
-    
+                scoreCal += Math.abs(parseInt(FriendScore) - parseInt(friendScores));
+                //console.log(scoreCal);
+            }
+            if (scoreCal <= match.scores) {
+                match.name = Friend.name;
+                match.photo = Friend.photo;
+                match.scores = scoreCal;
+            }
+        }
+        friends.push(userData);
+        res.json(match);
+    });
